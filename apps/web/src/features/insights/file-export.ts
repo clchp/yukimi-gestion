32f8f3482a448c58322073559a0f2ca4d6eef219
claudia <claudia.chauca@pucp.edu.pe>
2026-08-06 17:09:31 -0500

    fix: usar exportador XLSX compatible con Excel

--- apps/web/src/features/insights/file-export.ts
+++ apps/web/src/features/insights/file-export.ts
@@ -1,1 +1,3 @@
-export * from './file-export-v2';
+export type { BusinessPdfReport, PdfSection, WorkbookSheet } from './file-export-v2';
+export { downloadBusinessPdf, downloadPdf } from './file-export-v2';
+export { downloadXlsx } from './file-export-xlsx';
